a708b8265b43f5be2a5d39b69cb90bf3f1e91a9c
KunalG31 <59837332+KunalG31@users.noreply.github.com>
2022-06-29 16:53:25 -0400

    Initial commit

---
+++ services/CoursesSolution/CoursesApi/Domain/CoursesData.cs
@@ -0,0 +1,74 @@
+﻿using CoursesApi.Adapters;
+using CoursesApi.Models;
+
+using MongoDB.Driver;
+
+namespace CoursesApi.Domain;
+
+public class CoursesData
+{
+
+    private readonly CoursesMongoDbAdapter _adapter;
+    private readonly CoursesPublish _publisher;
+
+    public CoursesData(CoursesMongoDbAdapter adapter, CoursesPublish publisher)
+    {
+        _adapter = adapter;
+        _publisher = publisher;
+    }
+
+    public async Task<CollectionModel<GetCoursesItemModel>> GetAllCoursesAsync()
+    {
+        var projection = Builders<Course>.Projection.Expression(c => new GetCoursesItemModel {
+            Id = c.Id.ToString(),
+            Title = c.Title,
+            Description = c.Description,
+            Category = c.Category,
+            NumberOfDays = c.NumberOfDays
+            
+        });
+        var response = await _adapter.GetCourseCollection().Find(_ => true).Project(projection).ToListAsync();
+
+        return new CollectionModel<GetCoursesItemModel>
+        {
+            Data = response
+        };
+    }
+
+    public async Task<GetCoursesItemModel> AddCourseToServicesAsync(CourseCreateModel request)
+    {
+        var category = "Services";
+        return await SaveCourse(request, category);
+    }
+
+    private async Task<GetCoursesItemModel> SaveCourse(CourseCreateModel request, string category)
+    {
+        var course = new Course
+        {
+            Title = request.Title,
+            Description = request.Description,
+            Category = category,
+            NumberOfDays = request.NumberOfDays!.Value,
+            PositionInCategory = 0
+
+        };
+
+        await _adapter.GetCourseCollection().InsertOneAsync(course);
+        // await _adapter.GetCourseCollection().ReplaceOneAsync(c => c.Id == course.Id, course, new ReplaceOptions { IsUpsert = true});
+        var response = new GetCoursesItemModel
+        {
+            Id = course.Id.ToString(),
+            Title = course.Title,
+            Category = course.Category,
+            Description = course.Description
+        };
+
+        await _publisher.PublishCourseAsync(course);
+        return response;
+    }
+
+    public async Task<GetCoursesItemModel> AddCourseToAngularAsync(CourseCreateModel request)
+    {
+        return await SaveCourse(request, "Angular");
+    }
+}
---
+++ services/CoursesSolution/EnrollmentsApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using Dapr;
+namespace EnrollmentsApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("process-course")]
+        [Topic("courses", "course")]
+        public async Task<ActionResult> ProcessCourse([FromBody] Object course)
+        {
+            _logger.LogInformation("Got a course! Thanks!" + course.ToString());
+            // save it to my database.
+            return Ok();
+        }
+
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
